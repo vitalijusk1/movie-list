fdf3b03f348a86c4a7cff6883a766ef798cd1ae9
vitalijusk1 <73233231+vitalijusk1@users.noreply.github.com>
2026-08-27 12:28:01 +0300

    Refactor filter components to use useFilterParam hook
    - Created useFilterParam hook to handle URL parameter updates with automatic page reset
    - Replaced useSearchParams logic in GenreSelector with useFilterParam
    - Replaced useSearchParams logic in RatingSelector with useFilterParam
    - Replaced useSearchParams logic in SortSelector with useFilterParam
    - Simplified GenreSelector toggle logic using ternary operator
    - Removed selector configuration array from RatingSelector in favor of direct component

--- frontend/src/pages/MovieListPage/components/FilterPanel/components/GenreSelector/GenreSelector.tsx
+++ frontend/src/pages/MovieListPage/components/FilterPanel/components/GenreSelector/GenreSelector.tsx
@@ -1,28 +1,18 @@
-import { useSearchParams } from "react-router-dom";
+import useFilterParam from "../../../../../../hooks/useFilterParam";
 import styles from "./GenreSelector.module.css";
 import Button from "../../../../../../components/Button/Button";
 import { useAppSelector } from "../../../../../../store/hooks";
 
 const GenreSelector = () => {
   const genres = useAppSelector((state) => state.movies.genres);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const selectedIds = searchParams.get("genreIds")?.split(",") ?? [];
+  const [genreIds, setGenreIds] = useFilterParam("genreIds");
+  const selectedIds = genreIds ? genreIds.split(",") : [];
 
   const toggleGenre = (id: string) => {
-    const next = new URLSearchParams(searchParams);
-    const isAlreadySelected = selectedIds.includes(id);
-
-    if (isAlreadySelected) {
-      const withoutId = selectedIds.filter((g) => g !== id);
-      withoutId.length > 0
-        ? next.set("genreIds", withoutId.join(","))
-        : next.delete("genreIds");
-    } else {
-      next.set("genreIds", [...selectedIds, id].join(","));
-    }
-
-    next.set("page", "1");
-    setSearchParams(next);
+    const nextIds = selectedIds.includes(id)
+      ? selectedIds.filter((g) => g !== id)
+      : [...selectedIds, id];
+    setGenreIds(nextIds.join(","));
   };
 
   return (
